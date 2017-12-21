a1da38f1aa18849618d2e79d29e5c0c8b1f528cb
orwa337 <orwa.houry@outlook.de>
2017-12-21 14:31:46 +0100

    improve the code

--- src/index.js
+++ src/index.js
@@ -4,73 +4,233 @@ import 'bootstrap/js/src';
 import './styles.scss';
 import navbarTemplate from './templates/navbar.html';
 import modalTemplate from './templates/modal.html';
+import checkoutTemplate from './templates/checkout.html';
+import paymentMethodRadioTemplate from './templates/payment-method-radio.html';
 import mkCarousel from './carousel';
 import refreshProducts from './products';
 
-//  append navbar
+//  this is the function which is used when the page loads
 $(() => {
-  $('#root').append(modalTemplate)
-    .append(navbarTemplate);
+  // for convenience we create a jQuery object in which
+  //  we will be able to put the content of our pages
+  const $pageContent = $('<div class="page-content"></div>');
+
+  let categories = [];
+  let products = [];
+
+  function fillPageContent(wantedCategory = -1) {
+    $pageContent.empty();
+
+    // ------------------------------------------------------------
+    //  populate carousel with categories
+    const $carousel = mkCarousel(categories);
+    // we put the page element (carousel inside the $pageContent)
+    $pageContent.append($carousel);
+    $carousel.carousel();
+
+    //  Iterate over the categories and append to navbar
+    const $navbarNav = $('.navbar-nav')
+      .empty()
+      .append(`
+        <li class="nav-item">
+          <a class="nav-link" data-id="-1" data-name="All products" href="#">
+            All Products
+          </a>
+        </li>`);
+    categories.forEach((category, number) => {
+      $navbarNav
+        .append(`
+          <li class="nav-item">
+            <a class="nav-link" data-id="${number}" data-name="${category.name}" href="#">${category.name}</a>
+          </li>`);
+    });
+    // ------------------------------------------------------------
+    // append products-grid inside the $pageContent
+    $pageContent
+      .append(`<div class="infobox"><h2 id="infos">All products (${Object.keys(products).length})</h2></div>`)
+      .append('<div id="products-grid" class="container-fluid"></div>');
+
+    //  populate products-grid with products
+    $('#products-grid').append('<div class="row"></div>');
+    refreshProducts(products, wantedCategory);
+
+    // click event handler on nav-links
+    $('.nav-link').click((eventObj) => {
+      eventObj.preventDefault();
+      const { target } = eventObj;
+      const categoryId = target.getAttribute('data-id');
+      $('.navbar-nav .active').removeClass('active');
+      $(target).closest('li').addClass('active');
+
+      fillPageContent(categoryId);
+    });
+  }
+
+  $('#root')
+    // we keep that outside of the page content
+    // because when we click on product details
+    // we replace its content
+    // (rather than creating the whole modal again)
+    .append(modalTemplate)
+    // the navbar stays accross the pages so
+    // we keep it outside of the page content too
+    .append(navbarTemplate)
+    // we add the $pageContent here and
+    // we will modify its own content later
+    .append($pageContent);
+
+  // in order to handle errors in consistent manner
+  function handleAJAXError(xhr, status, error) {
+    $pageContent
+      .empty()
+      .append(`<div>Ajax Error categories: ${error}</div>`);
+  }
+
+  // the #cart element is located in the navbar
+  // (which has been add above)
   $('#cart').click(((e) => {
     e.preventDefault();
-    $('.shopping-cart').toggle('slow', (() => {
-    }));
+    $('.shopping-cart').toggle('slow');
   }));
-  //  read categories
+
+  // the checkout button is located in the navbar too
+  $('.checkout-proceed').click(() => {
+    // create a jQuery object filled with the checkoutTemplate
+    const $checkout = $(checkoutTemplate);
+    // get the user information from the local storage
+    const user = JSON.parse(localStorage.getItem('user'));
+    // change the field values accordingly
+    $checkout.find('[name="user-name"]').val(`${user.firstname} ${user.lastname}`);
+    $checkout.find('[name="user-street"]').val(user.street);
+    $checkout.find('[name="user-city"]').val(`${user.postal} ${user.city}`);
+
+    const storedProducts = JSON.parse(localStorage.getItem('cart'));
+    let total = 0;
+    const $productsList = $checkout.find('.products-list');
+    storedProducts.forEach((product) => {
+      $productsList.append(`<li>
+        <span class="product-name">${product.name}</span>
+        <span class="product-price">${product.price}€</span>
+        <span class="product-quantity">${product.quantity}</span>
+      </li>`);
+
+      total += parseInt(product.price, 10) * product.quantity;
+    });
+    $checkout.find('.cart-total').text(`Total: ${total}€`);
+
+    // we attach a click listener on the "buy" button
+    $checkout.find('.checkout-buy').click((evt) => {
+      // prevent the default behavior (wich would submit the form)
+      evt.preventDefault();
+      // we need a JSON string of the data to be sent
+      const data = JSON.stringify({
+        products: storedProducts,
+        user: {
+          name: $checkout.find('[name="user-name"]').val(),
+          street: $checkout.find('[name="user-street"]').val(),
+          city: $checkout.find('[name="user-city"]').val(),
+        },
+        payment_method: $checkout.find('[name="payment"]:checked').val(),
+      });
+      // to send a POST request to the server
+      $.ajax('http://localhost:9090/api/order', {
+        method: 'POST',
+        // the content-type of the request has to be application/json
+        // in order for the server to be able to read the body (of the request)
+        contentType: 'application/json',
+        data,
+      })
+        .done(() => {
+          $checkout
+            .empty()
+            .append(`<div class="alert alert-success">
+              The order has been placed!
+            </div>`);
+        })
+        .fail(() => {
+          $checkout
+            .empty()
+            .append(`<div class="alert alert-danger">
+              An error occured, sorry.
+            </div>`);
+        });
+    });
+
+
+    $checkout.find('.checkout-cancel').click(() => {
+      fillPageContent();
+    });
+
+
+    // we put the $checkout in the page
+    // (a text "Payment methods are loading" is visible)
+    $pageContent
+      .empty()
+      .append($checkout);
+
+    // we clear the .payment-methods (in which the loading text is)
+    // and keep a reference ($paymentMethods) for later access
+    const $paymentMethods = $checkout
+      .find('.payment-methods')
+      .empty();
+
+    $.ajax('//localhost:9090/api/payment_methods')
+      .done((data) => {
+        // for each payment methods returned by the API
+        data.forEach((paymentMethod) => {
+          // we create a jQuery object
+          const $paymentMethod = $(paymentMethodRadioTemplate);
+          // in which we alterate the wanted attributes...
+          $paymentMethod.find('input').attr('value', paymentMethod.id);
+          // ...and texts
+          // It's important to do the ".find()" on the smallest set possible
+          // if we have used .find() on $checkout,
+          // it would have unwanted side effects (because the $checkout has several 'span's)
+          $paymentMethod.find('span').text(paymentMethod.name);
+          // finally we append the payment method to its container
+          $paymentMethods.append($paymentMethod);
+        });
+      })
+      // we could also use the handleAJAXError here
+      .fail((xhr, status, error) => {
+        $paymentMethods.text(error.message);
+      });
+
+    // close the cart widget
+    $('.shopping-cart').hide('slow');
+  });
+
+
+  // we will trick the $pageContent to add a padding top
+  // equivalent to the navbar outer height
+  // (don't forget to remove styles which were supposed to do that in your SCSS)
+  $pageContent.css('padding-top', $('.navbar').outerHeight());
+
+  // read categories
   $.ajax('http://localhost:9090/api/categories')
-    .done((categories) => {
-      //  populate carousel with categories
-      const $carousel = mkCarousel(categories);
-      $('#root').append($carousel);
-      $carousel.carousel();
-
-      //  Iterate over the categories and append to navbar
-      categories.forEach((category, number) => {
-        $('.navbar-nav').append(`
-            <li class="nav-item">
-            <a class="nav-link" data-id="${number}" data-name="${category.name}" href="#">${category.name}</a>
-            </li>`);
-      });
+    .done((data) => {
+      // store the data in the variable declared above
+      categories = data;
+      fillPageContent();
     })
     //  or fail trying
-    .fail((xhr, status, error) => {
-      $('#root').append(`<div>Ajax Error categories: ${error}</div>`);
-    });
+    .fail(handleAJAXError);
 
-  //  ajax req and append products grid
+  // ajax req and append products grid
   $.ajax('http://localhost:9090/api/products')
-    .done((products) => {
-      //  append products-grid after carousel
-      $('#root').append(`<div class="infobox"><h2 id="infos">All products (${Object.keys(products).length})</h2></div>`);
-      $('#root').append('<div id="products-grid" class="container-fluid"></div>');
-      //  populate products-grid with products
-      $('#products-grid').append('<div class="row"></div>');
-      refreshProducts(products, '-1');
-      // click event handler on nav-links
-      $('.nav-link').click((eventObj) => {
-        eventObj.preventDefault();
-        const { target } = eventObj;
-        const linkName = target.getAttribute('data-id');
-        $('.navbar-nav .active').removeClass('active');
-        $(target).closest('li').addClass('active');
-        //  clean the products-grid and update the content
-        refreshProducts(products, linkName);
-      });
+    .done((data) => {
+      // store the data in the variable declared above
+      products = data;
+      fillPageContent();
     })
-
     //  or fail trying
-    .fail((xhr, status, error) => {
-      $('#root').append(`<div>Ajax Error products: ${error}</div>`);
-    });
-
+    .fail(handleAJAXError);
+  // randomly select one user from the database at the beginning,
+  // so that we have one user for ordering and checkout
   $.ajax('http://localhost:9090/api/customers')
     .done((customers) => {
-      const user = JSON.stringify(customers[5]);
+      const user = JSON.stringify(customers[Math.floor(Math.random(customers.length))]);
       localStorage.setItem('user', user);
-    })
-    //  or fail trying
-    .fail((xhr, status, error) => {
-      $('#root').append(`<div>Ajax Error categories: ${error}</div>`);
     });
   // End
 });
--- src/server/app.js
+++ src/server/app.js
@@ -1,83 +1,139 @@
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const router = express.Router;
+const Router = express.Router;
 const app = express();
 const cors = require('cors');
-var mysql = require('mysql');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+const frontendDirectoryPath = path.resolve(__dirname, './../static');
+
+console.log('static resource at: ' + frontendDirectoryPath);
+app.use(express.static(frontendDirectoryPath));
+app.use(cors());
+app.use(bodyParser());
+
 
 var con = mysql.createConnection({
-
   host: 'localhost',
   user: 'root',
   password: 'q',
   database: 'online_shop'
-
 });
 
-const frontendDirectoryPath = path.resolve(__dirname, './../static');
 
-console.log('satic resource at ' + frontendDirectoryPath);
-app.use(express.static(frontendDirectoryPath));
-app.use(cors());
-// always want to have / api /
-
-const apiRouter = new router();
+// always want to have /api in the beginning
+const apiRouter = new Router();
 app.use('/api', apiRouter);
+
 apiRouter.get('/', (req, res) => {
-    res.send({ 'shop-api': '1.0' });
+  res.send({'shop-api': '1.0'});
 });
 
 apiRouter.get('/products', (req, res) => {
-    con.query('select * from products', function(err, rows) {
-    if (err)
-      throw err;
-    res.json(rows)
-    });
+  con.query('select * from products', function(err, rows) {
+    if(err)
+      throw res.json( err );
+
+    //console.log( rows );
+    res.json( rows );
+  });
 });
 
 apiRouter.get('/categories', (req, res) => {
-    con.query('select * from product_categories', function(err, rows) {
-    if (err)
-      throw err;
-    res.json(rows)
-    });
+  con.query('select * from product_categories', function(err, rows) {
+    if(err)
+      throw res.json( err );
+
+    //console.log( rows );
+    res.json( rows );
+  });
 });
 
-apiRouter.get('/customers', function(req,res) {
-  con.query('select * from customers', function(err, rows) {
-    if (err)
-      throw err;
-    res.json(rows)
-    });
+apiRouter.get('/customers', function(req, res) {
+  con.query('select * from customers where active = 1', function(err, rows) {
+    if(err)
+      throw res.json( err );
+
+    //console.log( rows );
+    res.json( rows );
+  });
 });
 
-apiRouter.put('/activate/:userid', function (req, res) {
-  con.query('update customers set active = ? where id = ?', [req.body.status , req.params.userid], function(err, rows) {
-    if (err)
-      throw res.json(err);
-    console.log( rows );
-    res.json(rows);
-    });
+apiRouter.get('/payment_methods', function(req, res) {
+  con.query('select * from payment_methods', function(err, rows) {
+    if(err)
+      throw res.json( err );
+
+    //console.log( rows );
+    res.json( rows );
+  });
 });
 
-apiRouter.post('/user', function (req, res) {
-  con.query('select * from customers where email = ?',[req.body.email] , function(err, rows ) {
-      if (err)
-      throw res.json(err);
-      if (rows.length > 0) {
-        throw res.json({error: 'Email is already exist'});
+apiRouter.put('/activate/:userid', function(req, res) {
+  con.query('update customers set active = ? where id = ?', 
+    [req.body.status, req.params.userid],
+    function(err, rows) {
+    if(err)
+      throw res.json( err );
+
+    //console.log( rows );
+    res.json( rows );
+  }); 
+});
+
+apiRouter.post('/user', function(req, res) {
+  
+  con.query('select * from customers where email = ?',
+    [req.body.email], 
+    function(err, rows) {
+      if(err)
+        throw res.json(err);
+
+      if( rows.length > 0 ) {
+        res.json({error: 'Email already exists.'});
       }
       else {
-          con.query('insert into customers (firstname, lastname, birthdate, phone, city, street, email) values (?,?,?,?,?,?,?)',[req.body.firstname, req.body.lastname, req.body.birthdate, req.body.phone, req.body.city, req.body.street, req.body.email,] ,function(err, rows) {
-    if (err)
-      throw res.json(err);
-    console.log( rows );
-    res.json(rows);
-    });
-      }
-  });
+        con.query(`insert into customers (firstname, lastname, birthdate, phone, city, street, email) 
+          values (?, ?, ?, ?, ?, ?, ?)`,
+          [
+            req.body.firstname, 
+            req.body.lastname, 
+            req.body.birthdate, 
+            req.body.phone, 
+            req.body.city, 
+            req.body.street, 
+            req.body.email 
+          ],
+          function(err, rows) {
+            if(err)
+              throw res.json( err );
 
+            res.json( rows );
+          }
+        );
+      }
+    });
+});
+
+apiRouter.post('/order', function(req, res) {
+  /* postpone to january ...
+  con.query('insert into orders (customer_id, payment_id, created, paid) values (?, ?, now(), NULL)', [req.body.customerid, req.body.payment_id], function(err, rows) {
+    const newOrderId = rows.insertId;
+    var sql = 'insert into order_details () values ';
+    res.json(rows);
+  });
+  */
+
+  fs.writeFile(path.resolve(__dirname, './../orders/orders'+Date.now()+'.txt'), JSON.stringify(req.body),
+    (err) => {
+      if(err) 
+        res.json({ error: err });
+
+      res.json({success:'order saved'});
+    });
 });
 
 apiRouter.put('/user/:userid', function(req, res) {
@@ -86,33 +142,39 @@ apiRouter.put('/user/:userid', function(req, res) {
   var i = 1;
   var bodyLength = Object.keys(req.body).length;
   var values = [];
-  for (var field in req.body) {
+  for(var field in req.body) {
     sql += field + ' = ?';
-    if(i<bodyLength)
-      sql +=',';
+    if(i < bodyLength)
+      sql += ',';
     i++;
-    values.push( req.body[field]);
+    values.push( req.body[field] );
   }
-  sql += 'where id = ?';
-  values.push( req.params.userid);
-  con.query(sql,values, function(err, rows) {
-    if (err)
-      throw res.json(err);
-    console.log(rows);
-    res.json(rows);
+
+  sql += ' where id = ?';
+  values.push( req.params.userid );
+  con.query(sql, 
+    values,
+    function(err, rows) {
+    if(err)
+      throw res.json( err );
+
+    //console.log( rows );
+    res.json( rows );
+  });   
+});
+
+apiRouter.delete('/user/:userid', function(req, res) {
+  con.query('update customers set deleted = now() where id = ?', [req.params.userid], 
+    function(err, rows) {
+    if(err)
+      throw res.json( err );
+
+    //console.log( rows );
+    res.json( rows );
   });
 });
 
-apiRouter.delete('/user/:userid', function (req, res) {
-  con.query('update customers set deleted = now() where id = ?', [req.params.userid], function(err, rows) {
-    if (err)
-      throw res.json(err);
-    console.log( rows );
-    res.json(rows);
-    });
-});
-
-app.listen(9090, (err) => {
-    if (err) throw err;
-    console.log('server started on port 9090');
+app.listen( 9090, (err) => {
+  if(err) throw err;
+  console.log('Server started on port 9090');
 });
